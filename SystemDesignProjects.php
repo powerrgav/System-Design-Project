1a71ec50150ed7e439c5ddb0f29081f912ca78a5
powerrgav <powerrgav@gmail.com>
2025-09-19 12:05:32 -0400

    Add files to git

---
+++ SystemDesignProjects.php
@@ -0,0 +1,25 @@
+<html>
+    
+    <head>
+        <title>Proj Title</title>
+        <link rel="stylesheet" href="SystemDesignProjectCSS.css">
+    </head>
+
+    <body style="background-color: #ede1dd;">
+        <div>
+            <h1 style="padding-left: 75%;">heading</h1>
+        </div>
+        
+        <hr>
+
+        <div class="verticalLine"> 
+            <p>paragraphaisudfhpoasldfiu</p> 
+            <?php 
+                $name = "user";
+                echo "<p> hello, " . $name . "!</p>";
+            ?>
+        </div>
+
+    </body>
+
+</html>
